1dff995969b0b08667533674d53bd29d5cef0f0d
Umair <umair.ashraf@brainxtech.com>
2018-11-28 17:03:18 +0500

    extra files removed

--- format.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# ------------------------------------------------------
-# PhpStorm formatting script.
-# ------------------------------------------------------
-#
-
-IDE_BIN_HOME="${0%/*}"
-exec "$IDE_BIN_HOME/../MacOS/phpstorm" format "$@"
