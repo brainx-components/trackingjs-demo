f1ee048751b0b2950f8f49f990432a0a5c9edd7e
Umair <umair.ashraf@brainxtech.com>
2018-11-28 16:56:49 +0500

    first commit

---
+++ format.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# ------------------------------------------------------
+# PhpStorm formatting script.
+# ------------------------------------------------------
+#
+
+IDE_BIN_HOME="${0%/*}"
+exec "$IDE_BIN_HOME/../MacOS/phpstorm" format "$@"
